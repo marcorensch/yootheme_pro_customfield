d8144983d4492b90517ffd7834c5a401bf973984
Marco <marco.rensch@area59.ch>
2019-01-10 13:43:49 +0100

    initial version 1.0
    Two Render Options (block and Result table)

---
+++ nx_customfields/helper/helper.php
@@ -0,0 +1,23 @@
+<?php
+/*
+object(YOOtheme\Theme\BuilderElement)#1523 (9) { 
+    ["id"]=> string(6) "page#4" 
+    ["name"]=> string(0) "" 
+    ["type"]=> string(15) "nx_customfields" 
+    ["content"]=> string(0) "" 
+    ["props"]=> array(4) { 
+        ["field_select"]=> int(1) 
+        ["field_select_img"]=> string(3) "1-1" 
+        ["attrs"]=> array(0) { } 
+        ["class"]=> array(0) { } } 
+        ["children"]=> array(1) { 
+            [0]=> object(YOOtheme\Theme\BuilderElement)#1524 (9) { 
+                ["id"]=> string(0) "" 
+                ["name"]=> string(0) "" 
+                ["type"]=> string(20) "nx_customfields_item" 
+                ["content"]=> string(0) "" 
+                ["props"]=> array(3) { 
+                    ["cffieldname"]=> string(9) "introtext" 
+
+                    */
+                    ?>
